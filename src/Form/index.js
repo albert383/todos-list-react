7d193e1d41d97c8eaeb2a19af7fc5da3d4efe24b
Albert Ciesla <albert383@gmail.com>
2024-09-04 17:32:35 +0200

    task list without interactions yet

---
+++ src/Form/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Form = () => (
+    <form className="form ">
+        <input id="newTask" name="newTask" className="form__input " type="text"
+            placeholder="Co jest do zrobienia?" />
+        <button className="form__button " type="submit">Dodaj zadanie</button>
+    </form>
+);
+
+export default Form;
---
+++ src/Tasks/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+
+const Tasks = ({ tasks, hideDoneTasks }) => (
+  <ul className="section__list">
+    {tasks.map((task) => (
+      <li
+        className={`section__listItem {
+        task.done && hideDoneTasks ? "section__listItem--hidden" : ""}`}
+        key={task.id}>
+        <button className="section__button section__button--done">
+          {task.done ? "✔︎" : ""}
+        </button>
+        <span className={`section__listItemText 
+              {task.done ? "section__listItemText--done" : ""}`}>
+          {task.content}
+        </span>
+        <button className="section__button section__button--remove">
+          🗑️
+        </button>
+      </li>
+    ))}
+  </ul >
+);
+
+export default Tasks;
