7cfcf7f5fcfd1dff3d0bfaa1124b1e87150cd1ec
Albert Ciesla <albert383@gmail.com>
2024-09-08 19:26:16 +0200

    autofocus added to input

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Form = () => (
     <form className="form ">
         <input id="newTask" name="newTask" className="form__input " type="text"
-            placeholder="Co jest do zrobienia?" />
+            placeholder="Co jest do zrobienia?" autoFocus/>
         <button className="form__button " type="submit">Dodaj zadanie</button>
     </form>
 );
