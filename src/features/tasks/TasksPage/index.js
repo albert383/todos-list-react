a47713ecac643b83970ea049beb1bd9c3d29201c
Albert Ciesla <albert383@gmail.com>
2024-10-22 18:53:09 +0200

    Refactor FetchExampleTasksButton to handle loading and error states in TasksPage

--- src/features/tasks/TasksPage/index.js
+++ src/features/tasks/TasksPage/index.js
@@ -1,37 +1,15 @@
-import React, { useState } from "react";
-import { fetchExampleTasks } from "../tasksSlice";
-import { useDispatch } from "react-redux";
+import React from "react";
 import Form from "./Form";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
-import { ButtonStyled } from "./Buttons/styled";
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
 import { ContainerWrapper } from "../../../common/Container/styled";
 import Search from "./Search";
-
-
+import FetchExampleTasksButton from "./FetchExampleTasksButton";
 
 function TasksPage() {
-  const dispatch = useDispatch();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const handleFetchExampleTasks = async () => {
-    setLoading(true);
-    setError(false);
   
-    try {
-      
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      dispatch(fetchExampleTasks());
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
     <ContainerWrapper>
       <Header title="Lista zadań" />
@@ -40,13 +18,7 @@ function TasksPage() {
         body={<Form />}
         extraHeaderContent={
           <>
-            <ButtonStyled
-              onClick={handleFetchExampleTasks}
-              disabled={loading}
-            >
-              {loading ? "Ładowanie..." : "Pobierz przykładowe zadania"}
-            </ButtonStyled>
-            {error && <p style={{ color: "red" }}>Upps, pobieranie nie powiodło się. Spróbuj ponownie później.</p>}
+            <FetchExampleTasksButton />
           </>
         }
       />
