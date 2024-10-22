6158ba32464d681472016b5426aefde04cb86808
Albert Ciesla <albert383@gmail.com>
2024-10-22 18:57:43 +0200

    ButtonStyled moved to Button folder and renamed Button

--- src/features/tasks/TasksPage/Buttons/styled.js
+++ src/features/tasks/TasksPage/Buttons/styled.js
@@ -9,20 +9,3 @@ export const ButtonsWrapper = styled.div`
         flex-direction: column;
     }
 `;
-
-export const ButtonStyled = styled.button`
-    background-color: ${({ theme }) => theme.color.transparent};
-    color: ${({ theme }) => theme.color.teal};
-    border: none;
-    margin: 0 0 0 20px;
-    transition: color 1s;
-    cursor: pointer;
-
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        margin: 10px;
-    }
-
-    &:disabled {
-        color: ${({ theme }) => theme.color.dustyGray};
-    }
-`;
